7b87ec1031e2bfdf034a7d76bf652143d8de2649
Bilal <bilalsid0075@gmail.com>
2021-09-17 09:26:09 +0530

    First commit

---
+++ src/Code2.java
@@ -0,0 +1,24 @@
+//WAP to move all 0's to the end of an array. Maintain the relative order of the other (non-zero) array elements.
+//
+//Before:    0  0  1  0  3  0  5  0  6                                              
+//After:     1  3  5  6  0  0  0  0  0
+public class Code2 {
+	public static void arrayPushZero(int arr[],int l) {
+		int count=0;
+		for (int i=0;i<l;i++) {
+			if(arr[i]!=0)
+				arr[count++]=arr[i];
+		}
+		while(count<l)
+			arr[count++]=0;
+	}
+	public static void main(String[] args)
+	{
+		int[] arr= {0,0,1,0,3,0,5,0,6};
+		int l=arr.length;
+		arrayPushZero(arr,l);
+		for (int i=0;i<l;i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
---
+++ src/Hello.java
@@ -0,0 +1,27 @@
+//Prime Number Program
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args)
+	{
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter the initial range");
+	    int number1 = myObj.nextInt();
+	    System.out.println("Enter the final range");
+	    int number2 = myObj.nextInt();
+		primeNum(number1,number2);
+	}
+	public static void primeNum(int num1,int num2) {
+		for(int i=num1;i<num2;i++) {
+			int temp=0;
+			if (i==1)
+				continue;
+			for(int j=2;j<i;j++) {
+				if (i%j==0)
+					temp+=1;
+			}
+			if (temp==0)
+				System.out.println(i);
+		}
+	}
+}
